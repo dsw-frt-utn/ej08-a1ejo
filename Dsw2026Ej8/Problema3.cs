a9c3f655a2231460f6197dd308dd13bd879f04b6
Alejo <alejomedina107@gmail.com>
2026-05-03 23:19:19 -0300

    Resolucion problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValor = originalValue;
+            copiaValor++;
+
+            Product copiaProd = product;
+
+           copiaProd.Description = "Descripción Modificada";
+
+            return $"{originalValue}-{copiaValor}-{product.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string Description { get; set; }
+
+        public void ModificarDescripcion(string nuevaDescripcion)
+        {
+            Description = nuevaDescripcion;
+        }
+    }
+}
