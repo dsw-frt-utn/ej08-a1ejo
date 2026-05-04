35b527a7d47e404201cc62380f784a62cafe998f
Alejo <alejomedina107@gmail.com>
2026-05-03 23:02:27 -0300

    Resolucion problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total1;
+
+            if (quantity <= 0)
+            {
+                total1 = 0m;
+            }
+            else
+            {
+                total1 = quantity * unitPrice;
+            }
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total1
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
