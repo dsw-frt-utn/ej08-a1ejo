e15e000c3304f525452696200f6f71746bd4c2a8
Alejo <alejomedina107@gmail.com>
2026-05-03 23:42:55 -0300

    Resolucion problema 5

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal BaseAmount { get; set; }
+        public virtual decimal CalculateTotal()
+        {
+            return BaseAmount;
+        }
+    }
+}
---
+++ Dsw2026Ej8/WholesaleSale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return BaseAmount * 0.90m;
+        }
+    }
+}
